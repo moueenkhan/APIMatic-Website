94073f4e2aa257eedc82aeed052607640c3d0638
moueenkhan <manusuduzai@gmail.com>
2022-05-30 13:06:24 +0500

    add new tc

--- cypress/integration/APIMaticweb.js
+++ cypress/integration/APIMaticweb.js
@@ -13,9 +13,21 @@ context("File upload", () => {
 
   it.only("Landing page Testing", () => {
     cy.contains("Faster API Integration", { timeout: 10000 });
+
+    // cy.get(".btn-primary")
+    // .should("have.css", "background-color", "#0C7FF2 !important")
     cy.xpath(
       "/html/body/div[1]/div/section[1]/div[2]/div[1]/div/section/div/div/div/div/div/div/div/div[1]/a/span"
-    ).should("have.css", "-webkit-box-pack", "center", "justify-content", "center");
+    )
+      .should("have.css", "-webkit-box-pack", "center")
+      .should("have.css", "justify-content", "center");
+
+    cy.xpath(
+      "/html/body/div[1]/div/section[1]/div[2]/div[1]/div/section/div/div/div/div/div/div/div/div[1]/a/span/span[2]"
+    ).should("have.css", "display", "flex")
+    .should("have.css", "font-weight", "600")
+    .should("have.css", "order", "10")
+    //.should("have.css", "display", "inline-block")
 
     //       cy.get('input[type=color]')
     //     .invoke('val', '#ff0000')
